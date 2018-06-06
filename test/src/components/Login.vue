4e8807c0087c42c597a87254e21ba6381c97e6b0
kfq <18210166063@163.com>
2018-06-06 15:59:25 +0800

    0.1
    创建axios  基准路径

---
+++ test/src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<h1>登录</h1>
+		<ul>
+			<li>
+				<label for="">用户名</label>
+				<input type="text" v-model="username" id="name" />
+			</li>
+			<li>
+				<label for="">密码</label>
+				<input type="text" v-model="password" id="pw" />
+			</li>
+			<li><input type="button" value="提交" @click="login" /></li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	import { LoginServlet } from '../../api/api.js'
+	export default {
+		data() {
+			return {
+				username: "",
+				password: ""
+			}
+		},
+		methods: {
+			login: function() {
+				//调用登录接口
+				LoginServlet({
+					userName: this.username,
+					passWord: this.password
+				}).then(function(response) {
+					if(response.meta.status === 200){
+						// 跳转路由
+						this.$router.push({name:'home'});
+					}else {
+						console.log("")
+					}
+					 
+				})
+
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
